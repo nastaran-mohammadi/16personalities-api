12c47588c7a1e19ccbe9bb1061a436055533212f
Nastaran <ext_nastaran.mohammadi@malaysiaairlines.com>
2024-05-03 17:18:34 +0800

    chnaged

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,32 @@
 import express, { Request, Response, NextFunction } from "express"
-import cors from "cors"
-import morgan from "morgan"
-import routes from "./routes"
-import { PORT } from "@/config"
-import { HttpError } from "./utils/httpError"
+// import cors from "cors"
+// import morgan from "morgan"
+// import routes from "./routes"
+// import { PORT } from "@/config"
+// import { HttpError } from "./utils/httpError"
 
 const app = express()
+app.use(express.json());
 
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(morgan("dev"))
+// app.use(cors())
+// app.use(express.json())
+// app.use(express.urlencoded({ extended: false }))
+// app.use(morgan("dev"))
 
-app.use("/api", routes)
+// app.use("/api", routes)
 
-app.use((err: HttpError, req: Request, res: Response) => {
-  res.status(err.status || 500).json({
-    message: err.message,
-  })
+// app.use((err: HttpError, req: Request, res: Response) => {
+//   res.status(err.status || 500).json({
+//     message: err.message,
+//   })
+// })
+
+app.get('/questions', (req, res) => {
+  res.send('HELLO')
 })
 
-app.listen(PORT, async () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(3000, async () => {
+  console.log(`Server running on port ${3000}`)
 })
 
 export default app
